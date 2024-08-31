ff7b405557879ba90cbc44387649e51ce372137d
angelator312 <angel.raev2012@gmail.com>
2024-08-31 11:26:18 +0300

    fixed initialize variables

--- player_variables.gd
+++ player_variables.gd
@@ -1,25 +1,24 @@
 extends Node
 
 # For level 1
-#var maxJumps=1
-#var startLives=2
-#var defaultTimeToSword=5
-#var plusJump=0
-#
-#
+var maxJumps=1
+var startLives=2
+var defaultTimeToSword=5
+var plusJump=0
+
 ## For level 2
 #var maxJumps=3
 #var startLives=4
 #var defaultTimeToSword=5
 #var plusJump=0
 
-# For level 3
-var maxJumps=3
-var startLives=4
-var defaultTimeToSword=1
-var plusJump=0
-
 ## For level 3
+#var maxJumps=3
+#var startLives=4
+#var defaultTimeToSword=3
+#var plusJump=0
+
+## For level 4
 #var maxJumps=3
 #var startLives=4
 #var defaultTimeToSword=1.5
